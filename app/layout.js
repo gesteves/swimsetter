34d6eb2fc90b804dce99637e4138b44789cc78a4
Guillermo Esteves <g@gesteves.com>
2025-05-26 15:18:10 -0600

    Set max width

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.className} min-h-[100dvh]`}>
-      <body className="min-h-[100dvh]">
+    <html lang="en" className={`${geistSans.className} min-h-[100dvh] max-w-full`}>
+      <body className="min-h-[100dvh] max-w-full">
         {children}
       </body>
     </html>
