18615031f3a1c52b603338b94c9cd2f4dc2c0cee
Guillermo Esteves <g@gesteves.com>
2025-05-26 16:43:02 -0600

    Add Geist font

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Endless Pool Set Tracker",
@@ -22,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={geistSans.className}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
         {children}
       </body>
