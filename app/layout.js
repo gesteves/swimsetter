04140c4251b85bc83be5444fed9654e5860bd2ac
Guillermo Esteves <g@gesteves.com>
2025-05-26 16:43:57 -0600

    Fix warning

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,14 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
 
-
 export const metadata = {
   title: "Endless Pool Set Tracker",
   description: "Track your Endless Pool swim workouts by time and pace",
-  viewport: {
-    width: "device-width",
-    minimumScale: 1,
-  },
+};
+
+export const viewport = {
+  width: "device-width",
+  minimumScale: 1,
 };
 
 export default function RootLayout({ children }) {
