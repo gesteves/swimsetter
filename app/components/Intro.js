4c98438f89bdb887313137b3b7beec284213a863
Guillermo Esteves <g@gesteves.com>
2025-05-27 07:37:40 -0600

    Update Intro.js

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -8,7 +8,7 @@ export default function Intro() {
         <li><strong>Put your phone in a waterproof case</strong> and leave it on the side of the pool.</li>
         <li><strong>Start a timer or stopwatch</strong> on your watch.</li>
         <li><strong>Swim!</strong></li>
-        <li>Whenever you stop, <strong>tap “Add Set”</strong> and enter the time and pace of your last set.</li>
+        <li>Whenever you stop, <strong>tap “Add Set”</strong> and enter the duration and pace of your last set.</li>
         <li>When you’re done, <strong>enter the details</strong> in your workout log or Strava activity.</li>
       </ul>
     </Card>
