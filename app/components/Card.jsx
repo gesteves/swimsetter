3c40fc804869958f91f6b22e19ee0c39863f4c48
Guillermo Esteves <g@gesteves.com>
2025-05-30 09:13:08 -0600

    Reduce padding

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,11 +1,11 @@
 export default function Card({ children, footer, className = "" }) {
   return (
     <div className={`divide-y divide-gray-200 dark:divide-gray-700 overflow-hidden rounded-lg bg-white dark:bg-gray-800 shadow-sm ${className}`}>
-      <div className="px-4 py-5 sm:p-6">
+      <div className="p-4 sm:p-6">
         {children}
       </div>
       {footer && (
-        <div className="px-4 py-4 flex items-center justify-center">
+        <div className="p-4 flex items-center justify-center">
           {footer}
         </div>
       )}
