33215ea524fb062804e674da9dce4ce900b7a48c
Guillermo Esteves <g@gesteves.com>
2025-05-30 09:58:16 -0600

    Add hover background

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,8 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Card({ children, footer, footerButton, className = "" }) {
   const variantClasses = {
-    default: "text-blue-600 hover:text-blue-500",
-    danger: "text-red-600 hover:text-red-500",
+    default: "text-blue-600 hover:text-blue-500 hover:bg-blue-50 dark:hover:bg-blue-900",
+    danger: "text-red-600 hover:text-red-500 hover:bg-red-50 dark:hover:bg-red-900",
   };
 
   return (
@@ -11,13 +11,13 @@ export default function Card({ children, footer, footerButton, className = "" })
       <div className="p-4 sm:p-6">{children}</div>
 
       {(footer || footerButton) && (
-        <div className={`${footerButton ? "px-4 py-1" : "p-4"} flex items-center justify-center`}>
+        <div className={`${footerButton ? "p-1" : "p-4"} flex items-center justify-center`}>
           {footer ? (
             footer
           ) : (
             <button
               onClick={footerButton.onClick}
-              className={`text-sm w-full py-2 transition-colors ${
+              className={`text-sm w-full p-2 rounded-md transition-colors ${
                 variantClasses[footerButton.variant || "default"]
               }`}
             >
